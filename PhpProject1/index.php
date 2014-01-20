2542cb7e7255d7ced06ca720aa06e7b5754bb6b2
vuthy32 <vuthy32@gmail.com>
2014-01-20 23:23:11 +0700

    I add NEw Line Call  Test 3

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
+    <body>Test From Client
         <?php
         // put your code here
         ?>
