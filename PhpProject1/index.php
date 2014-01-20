2bcb07a1a5e3e32577208f6b1b0378549e36eaa4
vuthy323 <vuthy32@hotmail.com>
2014-01-20 23:28:51 +0700

    I add New File

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>Test From Client
+    <body>Test From Clientsdfsdfsdf
         <?php
         // put your code here
         ?>
